c6466d0ba2ea1ed59f7247e086466342f45ba243
LOSTHM <losthm@users.noreply.github.com>
2026-07-31 16:32:10 +0800

    fix: add import in WebhooksViewModel

--- app/src/main/java/com/luka/hermes/ui/WebhooksViewModel.kt
+++ app/src/main/java/com/luka/hermes/ui/WebhooksViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.buildJsonArray
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.add
 import kotlinx.serialization.json.contentOrNull
 import kotlinx.serialization.json.put
 
